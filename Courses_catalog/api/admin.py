401e1cf4425b94a08116edeed14ee727dfaa4977
Bohdan <bod.upkill@gmail.com>
2021-04-30 09:28:23 +0300

    added app to setting, fixed typo

--- Courses_catalog/api/admin.py
+++ Courses_catalog/api/admin.py
@@ -4,7 +4,7 @@ from .models import Courses
 
 # Register our model in admin.
 class CoursesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'start_data', 'end_date', 'amount')
+    list_display = ('id', 'name', 'start_date', 'end_date', 'amount')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
     list_filter = ('start_date', 'end_date')
